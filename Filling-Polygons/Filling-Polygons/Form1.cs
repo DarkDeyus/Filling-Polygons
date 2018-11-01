7d3d9420fb43cc6397001901143d786fcc441c06
DarkDev <21362600+DarkDeyus@users.noreply.github.com>
2018-11-01 19:01:28 +0100

    Start of the project

---
+++ Filling-Polygons/Filling-Polygons/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filling_Polygons
+{
+    public partial class Form1 : Form
+    {
+        List<Triangle> triangles;
+        Color lightSourceColor = Color.White;
+        public Form1()
+        {
+            InitializeComponent();
+            triangles = new List<Triangle>() { new Triangle(new Point(10, 10), new Point(100, 100), new Point(100, 400)) };
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Triangle triangle in triangles)
+            {
+                triangle.Paint(e);
+            }
+        }      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lightSourceColor = colorDialog1.Color;
+                button1.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
---
+++ Filling-Polygons/Filling-Polygons/Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filling_Polygons
+{
+    class Triangle
+    {
+        List<Point> vertices;
+        
+
+        int vertexSize = 10; 
+        public Triangle(Point v1 , Point v2, Point v3) => vertices = new List<Point>() { v1, v2, v3 };
+
+        public void Paint(PaintEventArgs e)
+        {
+            for(int i=0; i< vertices.Count; i++)
+            {
+                e.Graphics.DrawLine(Pens.Black, vertices[i], vertices[(i + 1) % vertices.Count]);
+                Rectangle rect = new Rectangle(vertices[i].X - vertexSize / 2, vertices[i].Y - vertexSize / 2, vertexSize, vertexSize);
+                e.Graphics.DrawEllipse(Pens.Black, rect);
+                e.Graphics.FillEllipse(Brushes.Black, rect);
+
+            }
+            //Rectangle v1 = new Rectangle(firstVertex.X - vertexSize / 2, firstVertex.Y - vertexSize / 2, vertexSize, vertexSize);
+            //Rectangle v2 = new Rectangle(secondVertex.X - vertexSize / 2, secondVertex.Y - vertexSize / 2, vertexSize, vertexSize);
+            //Rectangle v3 = new Rectangle(thirdVertex.X - vertexSize / 2, thirdVertex.Y - vertexSize / 2, vertexSize, vertexSize);
+
+            //e.Graphics.DrawLine(Pens.Black, firstVertex, secondVertex);
+            //e.Graphics.DrawLine(Pens.Black, secondVertex, thirdVertex);
+            //e.Graphics.DrawLine(Pens.Black, thirdVertex, firstVertex);
+
+            //e.Graphics.DrawEllipse(Pens.Black, v1);
+            //e.Graphics.FillEllipse(Brushes.Black, v1);
+            //e.Graphics.DrawEllipse(Pens.Black, v2);
+            //e.Graphics.FillEllipse(Brushes.Black, v2);
+            //e.Graphics.DrawEllipse(Pens.Black, v3);
+            //e.Graphics.FillEllipse(Brushes.Black, v3);
+
+        }
+    }
+}
